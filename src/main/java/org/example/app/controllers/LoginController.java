ef9f7f5633c18cf91b58d47865ace475a74248b2
Thiago Coelho <thiago.coelho158@gmail.com>
2025-07-03 22:59:02 -0300

    feat: create LoginController

--- src/main/java/org/example/app/controllers/LoginController.java
+++ src/main/java/org/example/app/controllers/LoginController.java
@@ -1,4 +1,16 @@
 package org.example.app.controllers;
 
+import org.example.app.models.User;
+import org.example.app.models.UserDAO;
+
 public class LoginController {
+    private static final UserDAO userDAO = new UserDAO();
+
+    public static User authenticate(String username, String password) {
+        User user = userDAO.findByUsername(username);
+        if (user != null && user.getPassword().equals(password)) {
+            return user;
+        }
+        return null;
+    }
 }
